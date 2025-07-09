d18baa861564711eece2bc57850d6953f7720ef7
Vinicius Yudi <vinizzly2005@gmail.com>
2025-07-09 19:37:24 -0300

    add -> rooms listing

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,10 +1,10 @@
-import postgres from "postgres";
-import { env } from "../env.ts";
-import { drizzle } from "drizzle-orm/postgres-js";
-import { schema } from "./schema/index.ts";
+import postgres from 'postgres';
+import { env } from '../env.ts';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { schema } from './schema/index.ts';
 
-export const sql = postgres(env.DATABASE_URL)
+export const sql = postgres(env.DATABASE_URL);
 export const db = drizzle(sql, {
-    schema,
-    casing: "snake_case"
-})
+  schema,
+  casing: 'snake_case',
+});
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,22 +1,22 @@
-import { reset, seed } from "drizzle-seed";
-import { db, sql } from "./connection.ts";
-import { schema } from "./schema/index.ts";
+import { reset, seed } from 'drizzle-seed';
+import { db, sql } from './connection.ts';
+import { schema } from './schema/index.ts';
 
-await reset(db, schema );
+await reset(db, schema);
 
-await seed(db, schema).refine(f => {
-    return {
-        rooms: {
-            count: 20,
-            columns: {
-                name: f.companyName(),
-                description: f.loremIpsum(),
-            }
-        }
-    }
-})
+await seed(db, schema).refine((f) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  };
+});
 
 await sql.end();
 
 // biome-ignore lint/suspicious/noConsole: only used in development
-console.log("Database seeded");
+console.log('Database seeded');
